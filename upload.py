38864c384ecefc2965439ac62d0abb774dc7884f
calmcl1 <calmcl1@aol.com>
2018-06-16 18:46:31 +0100

    dammit

--- upload.py
+++ upload.py
@@ -83,10 +83,10 @@ def do_upload_file(file_abs_location, b2_bucket_id):
         resp_data = json.loads(resp.read())
     except urllib2.HTTPError, e:
         print e.reason
-        print resp_data
+        #print resp_data
     except Exception, e:
         print e,
-        print resp_data
+        #print resp_data
 
 
 def generate_file_list(base_directory):
